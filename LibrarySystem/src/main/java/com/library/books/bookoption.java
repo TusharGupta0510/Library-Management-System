55ae198f7a84e2a0c301d182f589601c352cdaaa
Tushar Gupta <tushgupt1233@gmail.com>
2024-06-24 20:40:04 +0530

    updated commit

--- LibrarySystem/src/main/java/com/library/books/bookoption.java
+++ LibrarySystem/src/main/java/com/library/books/bookoption.java
@@ -1,5 +1,29 @@
 package com.library.books;
 
-public class bookoption {
+import java.util.*;
 
+public class bookoption{
+    public int sno;
+    public String bookname;
+    public String author;
+    public int qty;
+    public int qtycopy;
+
+    Scanner sc = new Scanner(System.in);
+
+    public bookoption() {
+        System.out.println("enter book number");
+        this.sno = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("enter book name");
+        this.bookname = sc.nextLine();
+
+        System.out.println("enter book author name");
+        this.author = sc.nextLine();
+
+        System.out.println("enter quantity");
+        this.qty = sc.nextInt();
+        qtycopy = this.qty;
+    }
 }
